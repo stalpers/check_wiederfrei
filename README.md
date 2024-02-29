2dca9f684e803c33b73e385842fa75cf5913dc56
Stefan Alpers <51530925+stalpers@users.noreply.github.com>
2024-02-29 07:52:41 +0100

    Refactored to use CSV for lookup

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-# NewDomains
+# NewDomains #
 
-Get List of newly .ch domains from twitter feed @wiederfrei and check their rank at urlmetriken.ch
+Get list of .ch & .li domains that got available recently domains from the Mastodon feed @wiederfrei and check their rank using Alexa Top1000 and Top 10Million Sites$
+
+Download and unzip the data files to the data directory 
+
+Alexa - http://s3-us-west-1.amazonaws.com/umbrella-static/top-1m.csv.zip
+Top 10 Million - https://www.domcop.com/files/top/top10milliondomains.csv.zip
+
+```
+├───data
+│   ├───alexa.csv
+│   └───top10milliondomains.csv
+├───Readme.md
+├───LICENSE
+└───check_released_domains.py
+
+```
+
+```
+python ./check_released_domains.py
+```
--- check_released_domains.py
+++ check_released_domains.py
@@ -1,83 +1,114 @@
-import re
+import os
+import pprint
+from collections import namedtuple
+import pickle
+import logging
 import requests
-import time
-import sys
+import requests
+import csv
 
-print ("[*] Checking <WiederFrei> Domain Names")
-html_file="C:\\tmp\\(6) Wieder Frei (@wiederfrei) _ Twitter.html"
+logging.basicConfig(format='%(asctime)s [%(levelname)s] - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-f = open(html_file, 'r', encoding="utf8")
-
-html_string = ""
-
-print("[*] Getting HTML")
-for x in f:
-  x.replace("\n","")
-  html_string = html_string + x
-
-ergebnis = []
+AlexaDB = namedtuple('AlexaDB', ['rank', 'domain'])
+Top10DB = namedtuple('Top10DB', ['rank', 'domain', 'open_rank'])
+Top10Results = namedtuple('Top10Results', ['rank', 'open_rank'])
 
 
-print("[*] Checking Status")
-for finding in re.findall(r'data-expanded-url="((http|https)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3})"', html_string):
-    url = "www." + finding[0].replace("http://","")
-    res = requests.get("http://urlmetriken.ch/" + url)
-    print ("[*] Please Wait ¦", end="")
-    for i in range(0, 10):
-        print(".", end="")
-        sys.stdout.flush()
-        time.sleep(0.5)
-
-    print("¦")
-    print("bla")
-    l = url + ";"
-    if res.status_code == 200:
-        print("[+] " + finding[0] + " - ", end="")
-        pos = re.findall(r'\"para_sent_top\">.+rangiert auf Platz ([\d.\'\\]+) in Schweiz',
-                         res.content.decode(res.encoding))
-        views = re.findall(r'<td class=.wh_two.>([\d\'\\]+)<\/td>', res.content.decode(res.encoding))
-        if pos:
-            print(str(pos[0]), end="")
-            l = l+ str(pos[0]) + ";"
-        else:
-            print ("Not Found")
-            l = l + "--" + ";"
+def search_alexa_rank(domain_name, alexa_data: list[AlexaDB]) -> int | None:
+    domain_name = domain_name.lower()  # convert to lower case for comparison
+    for site in alexa_data:
+        if site.domain.lower() == domain_name:  # also convert site.Domain to lower case here
+            return site.rank
+    return None
 
 
-        if views:
-            print(str(views[0]))
-            l = l + str(views[0])
-        else:
-
-            l = l + "--"
-    else:
-        print("[X] " + finding[0] + " - ", end="")
-        l = l + "--;--"
-        print ("Not in URL Metrik " + res.url)
-        if re.findall("removed",res.url):
-            print("[!] WARNING - Removed in URL")
-
-    ergebnis.append(l)
-
-w = open("result.txt", "a")
-
-for e in ergebnis:
-    w.write(e)
-    print(".", end="")
-
-w.close()
-
-# res = requests.get("http://urlmetriken.ch/www.google.com")
-# print ("Status Google: " + str(res.status_code))
-# pos = re.findall(r'\"para_sent_top\">.+rangiert auf Platz ([\d.\'\\]+) in Schweiz',res.content.decode(res.encoding))
-# if pos:
-#     print ("Position: " + str(pos[0]))
-# views = re.findall(r'<td class=.wh_two.>([\d\'\\]+)<\/td>',res.content.decode(res.encoding))
-# if views:
-#     print ("Views: " + str(views[0]))
-# else:
-#     print ("View Error")
-# res2 = requests.get("http://urlmetriken.ch/www.noroot.ch")
-# print ("Status No Root: " + str(res2.status_code))
+def search_top10_rank(domain_name, top10_data: list[Top10DB]) -> Top10Results | None:
+    domain_name = domain_name.lower()  # convert to lower case for comparison
+    for site in top10_data:
+        if site.domain.lower() == domain_name:  # also convert site.Domain to lower case here
+            result = Top10Results(rank=site.rank, open_rank=site.open_rank)
+            return result
+    return None
 
 
+def read_alexa_data(filename):
+    alexa_l = []
+    with open(filename, 'r') as file:  # @TODO: check for file exists
+        csv_reader = csv.reader(file)
+        alexa_l = [AlexaDB(rank=row[0], domain=row[1]) for row in csv_reader]
+    return alexa_l
+
+
+def read_top10_data(filename):
+    result = []
+    with open(filename, 'r') as file:  # @TODO: check for file exists
+        csv_reader = csv.reader(file)
+        # Skip the header
+        next(csv_reader)
+        result = [Top10DB(rank=row[0], domain=row[1], open_rank=row[2]) for row in csv_reader]
+    return result
+
+
+def get_domains():
+    contents_list = []
+    seen_domains_filename = 'seen_domains.pkl'
+
+    # If file exists, load the seen domains from it
+    if os.path.exists(seen_domains_filename):
+        with open(seen_domains_filename, 'rb') as seen_domains_file:
+            seen_domains = pickle.load(seen_domains_file)
+    else:  # Else, create an empty set
+        seen_domains = set()
+
+    url = "https://mastodon.social/api/v1/accounts/lookup?&acct=@wiederfrei@botsin.space"
+    response = requests.get(url)
+    data = response.json()
+    id_value = data.get('id')
+    url = f"https://mastodon.social/api/v1/accounts/{id_value}/statuses"
+    response = requests.get(url)
+    data = response.json()
+    # Loop through the returned statuses
+    not_seen_count = 0
+    count = 0
+    for status in data:
+        # Extract the 'content' field
+        content_field = status.get('content')
+        content_field = content_field.replace(' sind wieder frei!</p>', '')
+        content_field = content_field.replace(' ist wieder frei!</p>', '')
+        content_field = content_field.replace('<p>', '').replace(' ', '')
+        separated_values = content_field.replace('und', ',').split(',')
+
+        count = count + len(separated_values)
+        for value in separated_values:
+
+            if value not in seen_domains:
+                contents_list.append(value)
+                seen_domains.add(value)
+                not_seen_count += 1
+
+        # contents_list.extend(separated_values)
+        seen_domains_file = open(seen_domains_filename, 'wb')
+        pickle.dump(seen_domains, seen_domains_file)
+        seen_domains_file.close()
+    logger.info(f'{not_seen_count} New Domains - {count} total downloaded')
+    return contents_list
+
+
+if __name__ == "__main__":
+    logger.info("Loading Alexa data")
+    alexa = read_alexa_data('data/alexa.csv')
+    logger.info("Loading Open Page Rank data")
+    top10 = read_top10_data('data/top10milliondomains.csv')
+    logger.info("Getting domain information from Mastodon")
+    domains = get_domains()
+
+    for domain in domains:
+        a = search_alexa_rank(domain_name=domain, alexa_data=alexa)
+        t = search_top10_rank(domain_name=domain, top10_data=top10)
+        r = 'not available'
+        o = 'not available'
+        if t is not None:
+            r = t.rank
+            o = t.open_rank
+        print(f'{domain}\t{a}\t{r}\t{o}')
